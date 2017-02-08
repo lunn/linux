728de7a246c9ff44d4d3cef1e53d4a120f4e708b
Andrew Lunn <andrew@lunn.ch>
2017-02-07 20:50:13 -0600

    net: phy: phy_device: Call into the PHY driver to set LED brightness.
    Linux LEDs can be software controlled via the brightness file in /sys.
    LED drivers need to implement a brightness_set function which the core
    will call. Implement an intermediary in phy_device, which will call
    into the phy driver if it implements the necessary function.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -2310,7 +2310,18 @@ static bool phy_drv_supports_irq(struct phy_driver *phydrv)
 static int phy_led_set_brightness(struct led_classdev *led_cdev,
 				  enum led_brightness value)
 {
-	return 0;
+	struct phy_led *phyled = to_phy_led(led_cdev);
+	struct phy_device *phydev = phyled->phydev;
+	int err;
+
+	if (!phydev->drv->led_brightness_set)
+		return -ENOTSUPP;
+
+	mutex_lock(&phydev->lock);
+	err = phydev->drv->led_brightness_set(phydev, phyled->index, value);
+	mutex_unlock(&phydev->lock);
+
+	return err;
 }
 
 static int of_phy_led(struct phy_device *phydev,
@@ -2342,7 +2353,8 @@ static int of_phy_led(struct phy_device *phydev,
 	if (err && err != -EINVAL)
 		return err;
 
-	cdev->brightness_set_blocking = phy_led_set_brightness;
+	if (phydev->drv->led_brightness_set)
+		cdev->brightness_set_blocking = phy_led_set_brightness;
 
 	err = devm_led_classdev_register(dev, cdev);
 	if (err)
--- include/linux/phy.h
+++ include/linux/phy.h
@@ -461,6 +461,8 @@ struct phy_led {
 	u32 index;
 };
 
+#define to_phy_led(d) container_of(d, struct phy_led, led_cdev)
+
 /* struct phy_driver: Driver structure for a particular PHY type
  *
  * driver_data: static driver data
@@ -644,8 +646,17 @@ struct phy_driver {
 	int (*set_tunable)(struct phy_device *dev,
 			    struct ethtool_tunable *tuna,
 			    const void *data);
+
 	int (*set_loopback)(struct phy_device *dev, bool enable);
+	/* Set a PHY LED brightness. Index indicates which of the PHYs
+	 * led should be set. Value follows the standard LED class meaning,
+	 * e.g. LED_OFF, LED_HALF, LED_FULL.
+	 */
+
+	int (*led_brightness_set)(struct phy_device *dev,
+				  u32 index, enum led_brightness value);
 };
+
 #define to_phy_driver(d) container_of(to_mdio_common_driver(d),		\
 				      struct phy_driver, mdiodrv)
 
