33a387bbe10c6d608f005018ce5bc33db8ba28da
Andrew Lunn <andrew@lunn.ch>
2017-10-04 22:18:45 -0500

    dsa: Pass the port to get_sset_count()
    By passing the port, we allow different ports to have different
    statistics. This is useful since some ports have SERDES interfaces
    with their own statistic counters.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/dsa/b53/b53_priv.h
+++ drivers/net/dsa/b53/b53_priv.h
@@ -287,7 +287,7 @@ static inline int b53_switch_get_reset_gpio(struct b53_device *dev)
 void b53_imp_vlan_setup(struct dsa_switch *ds, int cpu_port);
 void b53_get_strings(struct dsa_switch *ds, int port, uint8_t *data);
 void b53_get_ethtool_stats(struct dsa_switch *ds, int port, uint64_t *data);
-int b53_get_sset_count(struct dsa_switch *ds);
+int b53_get_sset_count(struct dsa_switch *ds, int port);
 int b53_br_join(struct dsa_switch *ds, int port, struct net_device *bridge);
 void b53_br_leave(struct dsa_switch *ds, int port, struct net_device *bridge);
 void b53_br_set_stp_state(struct dsa_switch *ds, int port, u8 state);
