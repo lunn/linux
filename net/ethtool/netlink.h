f69232a49384b0a801a2615cb6cf6736682a2118
Michal Kubecek <mkubecek@suse.cz>
2017-11-10 15:12:24 +0100

    ethtool: introduce ethtool netlink interface
    Basic genetlink and init infrastructure for the netlink interface, register
    genetlink family "ethtool". Introduce CONFIG_ETHTOOL_NETLINK Kconfig
    option. Add interface description into Documentation/networking.
    
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>

---
+++ net/ethtool/netlink.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+
+#ifndef _NET_ETHTOOL_NETLINK_H
+#define _NET_ETHTOOL_NETLINK_H
+
+#include <linux/ethtool_netlink.h>
+#include <linux/netdevice.h>
+#include <net/genetlink.h>
+
+extern struct genl_family ethtool_genl_family;
+
+#endif /* _NET_ETHTOOL_NETLINK_H */
