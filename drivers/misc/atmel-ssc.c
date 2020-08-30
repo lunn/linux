28d220575642976f07a8cd9930eefc42ca66670c
Andrew Lunn <andrew@lunn.ch>
2020-08-30 10:48:32 -0500

    misc: atmel-ssc: Remove unneeded  #ifdef CONFIG_OF
    Now that the of_match_node() stub is an inline function, there is no
    need to #ifdef out the match table. The compiler will see the match
    table is unused and optimise it out.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/misc/atmel-ssc.c
+++ drivers/misc/atmel-ssc.c
@@ -111,7 +111,6 @@ static const struct platform_device_id atmel_ssc_devtypes[] = {
 	}
 };
 
-#ifdef CONFIG_OF
 static const struct of_device_id atmel_ssc_dt_ids[] = {
 	{
 		.compatible = "atmel,at91rm9200-ssc",
@@ -127,7 +126,6 @@ static const struct of_device_id atmel_ssc_dt_ids[] = {
 	}
 };
 MODULE_DEVICE_TABLE(of, atmel_ssc_dt_ids);
-#endif
 
 static inline const struct atmel_ssc_platform_data *
 	atmel_ssc_get_driver_data(struct platform_device *pdev)
