4b33345d5504079b1b40b0f1482639f58626cd86
Andrew Lunn <andrew@lunn.ch>
2021-11-20 12:56:35 -0600

    platform/x86: Add Zodiac Radio Access Point platform driver
    The device uses a CompExpress module on a baseboard which has a
    Marvell Ethernet switch and a number of Atheros wireless access point
    chipsets. Add a platform driver, which matches on Zodiac custom BIOS
    DMS strings and instantiates the needed drivers for the platform.
    
    NOT FOR MAINLINE
    
    platform: x86: zii-rap: Add TQ-Systems DMI Strings
    
    Hardware still exists which have the TQ-Sysems DMI Strings. Add them
    back to allow testing on such systems.
    
    NOT FOR MAINLINE

--- drivers/mfd/tqmx86.c
+++ drivers/mfd/tqmx86.c
@@ -253,6 +253,16 @@ static const struct dmi_system_id tqmx86_dmi_table[] __initconst = {
 		},
 		.callback = tqmx86_create_platform_device,
 	},
+	{
+		.ident = "RaveAP0101",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Safran"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "RaveAP"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "01"),
+			DMI_MATCH(DMI_CHASSIS_VERSION, "01"),
+		},
+		.callback = tqmx86_create_platform_device,
+	},
 	{}
 };
 MODULE_DEVICE_TABLE(dmi, tqmx86_dmi_table);
