53d3f1efdf2bc27927e07bcb2e08ed737b37b1b2
Andrew Lunn <andrew@lunn.ch>
2021-11-20 12:56:35 -0600

    platform/x86: Add Zodiac Radio Access Point platform driver
    The device uses a CompExpress module on a baseboard which has a
    Marvell Ethernet switch and a number of Atheros wireless access point
    chipsets. Add a platform driver, which matches on Zodiac custom BIOS
    DMS strings and instantiates the needed drivers for the platform.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    
    ZII: Use existing gpio_decoder input driver
    
    The platform driver effectively re-implemented the input driver
    gpio_decoder.  Swap to using this driver. This however means the input
    device changes name, and the file in /sys is removed.
    
    mfd: tqmx86: Add support for Zodiacs DMI IDs
    
    Zodiac makes a produced based on the TQ-Systems ComExpress module.  It
    has customised BIOS strings which are unique to the Zodiac system. In
    order that the tqmx86 driver loads, extend the driver with the Zodiac
    DMI IDs.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    
    rtc: ds1307: Power saving settings
    
    Configure chip to disable oscillator fault detection circuitry.
    Configure chip to enable crystal oscillator output glitch filtering
    
    These reduce the power consumption of the RTC at the expense of
    reliability and quality of service. In one measured case case, a
    DS1341 is powered by a supercap and enabling those two setting allows
    to increase holdup from less than a day do 2+ weeks.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    
    ath: regd: HACK the maximum transmit power to lower it
    
    The RAP is not allowed to transmit at the maximum power defined
    regulators. HACK the regulatory tables to limit the EIRP to 13dbM.
    
    NOT FOR MAINLINE
    
    platform: x86: zii-rap: Add TQ-Systems DMI Strings
    
    Hardware still exists which have the TQ-Sysems DMI Strings. Add them
    back to allow testing on such systems.
    
    NOT FOR MAINLINE

--- drivers/rtc/rtc-ds1307.c
+++ drivers/rtc/rtc-ds1307.c
@@ -84,6 +84,7 @@ enum ds_type {
 #define DS1337_REG_CONTROL	0x0e
 #	define DS1337_BIT_nEOSC		0x80
 #	define DS1339_BIT_BBSQI		0x20
+#	define DS1341_BIT_EGFIL		0x20
 #	define DS3231_BIT_BBSQW		0x40 /* same as BBSQI */
 #	define DS1337_BIT_RS2		0x10
 #	define DS1337_BIT_RS1		0x08
@@ -99,7 +100,9 @@ enum ds_type {
 #	define DS1340_BIT_OSF		0x80
 #define DS1337_REG_STATUS	0x0f
 #	define DS1337_BIT_OSF		0x80
+#	define DS1341_BIT_DOSF		0x40
 #	define DS3231_BIT_EN32KHZ	0x08
+#	define DS1341_BIT_ECLK		0x04
 #	define DS1337_BIT_A2I		0x02
 #	define DS1337_BIT_A1I		0x01
 #define DS1339_REG_ALARM1_SECS	0x07
@@ -1780,9 +1783,15 @@ static int ds1307_probe(struct i2c_client *client,
 		ds1307_can_wakeup_device = true;
 
 	switch (ds1307->type) {
+	case ds_1341:
+		regmap_update_bits(ds1307->regmap, DS1337_REG_CONTROL,
+				   DS1341_BIT_EGFIL, 0);
+		regmap_update_bits(ds1307->regmap, DS1337_REG_STATUS,
+				   DS1341_BIT_DOSF | DS1341_BIT_ECLK,
+				   DS1341_BIT_DOSF);
+		fallthrough;
 	case ds_1337:
 	case ds_1339:
-	case ds_1341:
 	case ds_3231:
 		/* get registers that the "rtc" read below won't read... */
 		err = regmap_bulk_read(ds1307->regmap, DS1337_REG_CONTROL,
