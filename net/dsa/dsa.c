79f2b6fdc9a3cbc806fe12623d30a0c1ad847159
Andrew Lunn <andrew@lunn.ch>
2022-09-19 07:09:21 -0500

    net: dsa: qca8: Move inband mutex into DSA core
    The mutex serves two purposes:
    
    It serialises operations on the switch, so that only one
    request/response can be happening at once.
    
    It protects priv->mgmt_master, which itself has two purposes.  If the
    hardware is wrongly wired, the wrong switch port is connected to the
    cpu, inband cannot be used. In this case it has the value
    NULL. Additionally, if the master is down, it is set to NULL.
    Otherwise it points to the netdev used to send frames to the switch.
    
    The protection of priv->mgmt_master is not required. It is a single
    pointer, which will be updated atomically. It is not expected that the
    interface disappears, it only goes down. Hence mgmt_master will always
    be valid, or NULL.
    
    Move the check for the master device being NULL into the core.  Also,
    move the mutex for serialisation into the core.
    
    The MIB operations don't follow request/response semantics, so its
    mutex is left untouched.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- net/dsa/dsa.c
+++ net/dsa/dsa.c
@@ -521,6 +521,7 @@ EXPORT_SYMBOL_GPL(dsa_mdb_present_in_other_db);
 void dsa_inband_init(struct dsa_inband *inband, u32 seqno_mask)
 {
 	init_completion(&inband->completion);
+	mutex_init(&inband->lock);
 	spin_lock_init(&inband->resp_lock);
 	inband->seqno_mask = seqno_mask;
 	inband->seqno = 0;
@@ -566,6 +567,11 @@ int dsa_inband_request(struct dsa_inband *inband, struct sk_buff *skb,
 
 	inband->err = 0;
 
+	if (!skb->dev)
+		return -EOPNOTSUPP;
+
+	mutex_lock(&inband->lock);
+
 	spin_lock(&inband->resp_lock);
 	inband->resp = resp;
 	inband->resp_len = resp_len;
@@ -586,6 +592,7 @@ int dsa_inband_request(struct dsa_inband *inband, struct sk_buff *skb,
 	inband->resp = NULL;
 	inband->resp_len = 0;
 	spin_unlock(&inband->resp_lock);
+	mutex_unlock(&inband->lock);
 
 	if (ret == 0)
 		return -ETIMEDOUT;
