1788fc1805aba72ce311678639194a969121fb72
Andrew Lunn <andrew@lunn.ch>
2022-11-04 18:42:14 -0500

    dsa: mv88e6xxx: Add RMU register read/write/wait for bit
    Add support for reading a register, writing a register, and waiting
    for a bit in a register to change, via the remote management unit.
    
    If the RMU is not enabled, return -EOPNOTSUPP, and fall back to MDIO.
    Additionally, if the operation times out, fall back to MDIO. Other
    errors, such as protocol errors are however reported, rather than
    falling back to MDIO.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/dsa/mv88e6xxx/rmu.h
+++ drivers/net/dsa/mv88e6xxx/rmu.h
@@ -15,12 +15,39 @@
 #define MV88E6XXX_RMU_REQ_FORMAT_SOHO		htons(0x0001)
 #define MV88E6XXX_RMU_REQ_PAD			htons(0x0000)
 #define MV88E6XXX_RMU_REQ_CODE_GET_ID		htons(0x0000)
+#define MV88E6XXX_RMU_REQ_CODE_REG_RW		htons(0x2000)
 #define MV88E6XXX_RMU_REQ_DATA			htons(0x0000)
+
+#define MV88E6XXX_RMU_REQ_RW_0_OP_WAIT_1	(0x3 << 10)
+#define MV88E6XXX_RMU_REQ_RW_0_OP_READ		(0x2 << 10)
+#define MV88E6XXX_RMU_REQ_RW_0_OP_WRITE		(0x1 << 10)
+#define MV88E6XXX_RMU_REQ_RW_0_OP_WAIT_0	(0x0 << 10)
+
+#define MV88E6XXX_RMU_REQ_RW_0_READ(_addr_, _reg_)		 \
+	htons(MV88E6XXX_RMU_REQ_RW_0_OP_READ |			 \
+	      ((_addr_) << 5) |					 \
+	      (_reg_))
+#define MV88E6XXX_RMU_REQ_RW_0_WRITE(_addr_, _reg_)		 \
+	htons(MV88E6XXX_RMU_REQ_RW_0_OP_WRITE |			 \
+	      ((_addr_) << 5) |					 \
+	      (_reg_))
+
+#define MV88E6XXX_RMU_REQ_RW_0_WAIT_0(_addr_, _reg_)		   \
+	htons(MV88E6XXX_RMU_REQ_RW_0_OP_WAIT_0 |		   \
+	      ((_addr_) << 5) |					   \
+	      (_reg_))
+#define MV88E6XXX_RMU_REQ_RW_0_WAIT_1(_addr_, _reg_)		   \
+	htons(MV88E6XXX_RMU_REQ_RW_0_OP_WAIT_1 |		   \
+	      ((_addr_) << 5) |					   \
+	      (_reg_))
+
+#define MV88E6XXX_RMU_REQ_RW_0_END		htons(0xffff)
+#define MV88E6XXX_RMU_REQ_RW_1_END		htons(0xffff)
 
 #define MV88E6XXX_RMU_RESP_FORMAT_1		htons(0x0001)
 #define MV88E6XXX_RMU_RESP_FORMAT_2		htons(0x0002)
-
 #define MV88E6XXX_RMU_RESP_CODE_GOT_ID		htons(0x0000)
+#define MV88E6XXX_RMU_RESP_CODE_REG_RW		htons(0x2000)
 
 struct mv88e6xxx_rmu_header {
 	__be16 format;
@@ -28,6 +55,20 @@ struct mv88e6xxx_rmu_header {
 	__be16 code;
 } __packed;
 
+struct mv88e6xxx_rmu_rw_resp {
+	struct mv88e6xxx_rmu_header rmu_header;
+	__be16 cmd;
+	__be16 value;
+	__be16 end0;
+	__be16 end1;
+} __packed;
+
+int mv88e6xxx_rmu_write(struct mv88e6xxx_chip *chip, int addr, int reg,
+			u16 val);
+int mv88e6xxx_rmu_read(struct mv88e6xxx_chip *chip, int addr, int reg,
+		       u16 *val);
+int mv88e6xxx_rmu_wait_bit(struct mv88e6xxx_chip *chip, int addr, int reg,
+			   int bit, int val);
 void mv88e6xxx_rmu_conduit_state_change(struct dsa_switch *ds,
 					const struct net_device *master,
 					bool operational);
