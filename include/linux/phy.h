dece1b730ffd6c0899a75965d5b115f3f558654f
Andrew Lunn <andrew@lunn.ch>
2023-02-15 15:55:45 -0600

    net: phy: Add helper to set EEE Clock stop enable bit
    The MAC driver can request that the PHY stops the clock during EEE
    LPI. This has normally been does as part of phy_init_eee(), however
    that function is overly complex and often wrongly used. Add a
    standalone helper, to aid removing phy_init_eee().
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    ---
    v2: Add missing EXPORT_SYMBOL_GPL

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1925,6 +1925,7 @@ int phy_unregister_fixup_for_id(const char *bus_id);
 int phy_unregister_fixup_for_uid(u32 phy_uid, u32 phy_uid_mask);
 
 int phy_init_eee(struct phy_device *phydev, bool clk_stop_enable);
+int phy_eee_clk_stop_enable(struct phy_device *phydev);
 int phy_get_eee_err(struct phy_device *phydev);
 int phy_ethtool_set_eee(struct phy_device *phydev, struct ethtool_eee *data);
 int phy_ethtool_get_eee(struct phy_device *phydev, struct ethtool_eee *data);
