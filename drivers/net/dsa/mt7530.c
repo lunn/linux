ddd317e71c36617e1b8fbee5899dee6a4eb9c05b
Andrew Lunn <andrew@lunn.ch>
2023-02-15 20:38:54 -0600

    net: dsa: mt7530: Swap to using phydev->eee_active
    Rather than calling phy_init_eee() retrieve the same information from
    within the phydev structure.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -2753,7 +2753,7 @@ static void mt753x_phylink_mac_link_up(struct dsa_switch *ds, int port,
 			mcr |= PMCR_RX_FC_EN;
 	}
 
-	if (mode == MLO_AN_PHY && phydev && phy_init_eee(phydev, false) >= 0) {
+	if (mode == MLO_AN_PHY && phydev && phydev->eee_active) {
 		switch (speed) {
 		case SPEED_1000:
 			mcr |= PMCR_FORCE_EEE1G;
